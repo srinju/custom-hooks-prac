101ad96f97f7af7a9b8f5b1fc17540f65e7ff504
SRINJOY DAS <115316528+srinju@users.noreply.github.com>
2024-08-15 16:47:33 +0530

    Update App2.jsx

--- App2.jsx
+++ App2.jsx
@@ -2,17 +2,30 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios';
 
-function useTodos() {
+function useTodos(n) {
   const [todos , setTodos] = useState([]);
   const [loading,setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get("https://sum-server.100xdevs.com/todos")
+    const value = setInterval(() => { //what it does is that it fetches the random set of todos in every second the most updated way to do is with websockets in todays world but for understanding we are using this old ass setinterval method
+      axios.get("https://sum-server.100xdevs.com/todos")
       .then(res => {
         setTodos(res.data.todos);
         setLoading(false); //when data fetched from backend then obviously the loading will be set to false
       })
-  },[]);
+    }, n * 1000);
+
+    axios.get("https://sum-server.100xdevs.com/todos") //run this once then the upper code will run on every 5 seconds we are running this because in the beginning the user will not see any code in the beginning of 5 seconds
+      .then(res => {
+        setTodos(res.data.todos);
+        setLoading(false); //when data fetched from backend then obviously the loading will be set to false
+      })
+
+      return () => {
+        clearInterval(value); //we put the setinterval in a variable called value and here we are passing thaat value inside the clear interval function which basically clears the old interval and whenver an another interval is started the old interval is shutted down 
+      }
+
+  },[n]); //we are putting n as a dependency here that means whenver the n changes then basically the interval runs again so that becomes 2 intervals so when the n changes the new interval will start runniing and we have to clear the old interval if we dont then there will be 2 clocks running that is very much unoptimized and hitting the backend more often than ususal so the above return is the clean up function 
 
   return {todos,loading};
 }
@@ -20,7 +33,7 @@ function useTodos() {
 
 function App() {
 
-  const {todos,loading} = useTodos();
+  const {todos,loading} = useTodos(5);
 
   if (loading) {
     return <div>Loading ...</div>
